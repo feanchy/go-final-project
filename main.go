2d9f2bbbd4c04a09df23ab9842be6999cef882a8
feanchy <mgusejn007@gmail.com>
2026-07-10 00:25:47 +0300

    add db

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "go1f/pkg/server"
+import (
+	"go1f/pkg/db"
+	"go1f/pkg/server"
+	"log"
+	"os"
+)
 
 func main() {
-	err := server.Run()
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+
+	err := db.Init(dbFile)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	err = server.Run()
+	if err != nil {
+		log.Fatal()
 	}
 }
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,5 +1,52 @@
 package db
 
 import (
+	"database/sql"
+	"os"
+
 	_ "modernc.org/sqlite"
 )
+
+var db *sql.DB
+
+const schema = `
+CREATE TABLE scheduler (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date CHAR(8) NOT NULL DEFAULT "",
+title VARCHAR(256) NOT NULL DEFAULT "",
+comment TEXT NOT NULL DEFAULT "",
+repeat VARCHAR(128) NOT NULL DEFAULT ""
+);
+
+CREATE INDEX idx_scheduler_date
+ON scheduler(date);
+`
+
+func Init(dbFile string) error {
+	_, err := os.Stat(dbFile)
+
+	install := false
+	if err != nil {
+		install = true
+	}
+
+	db, err = sql.Open("sqlite", dbFile)
+	if err != nil {
+		return err
+	}
+
+	if install {
+		_, err = db.Exec(schema)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Close() error {
+	if db != nil {
+		return db.Close()
+	}
+	return nil
+}
